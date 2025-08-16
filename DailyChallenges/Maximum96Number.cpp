da7c125617060d273254517fe68ca94c37ff6088
TayssirGh <tayssir.ghanmi@eniso.u-sousse.tn>
2025-08-16 12:19:57 +0200

    DAILY CHALLENGE: maximum 69 number [https://leetcode.com/problems/maximum-69-number]

---
+++ DailyChallenges/Maximum96Number.cpp
@@ -0,0 +1,19 @@
+//
+// Created by tayssir on 8/16/25.
+//
+#include <bits/stdc++.h>
+using namespace std;
+int maximum69Number (int num) {
+    string s = to_string(num);
+    for(int i = 0; i < s.length(); i++) {
+        if(s[i] == '6') {
+            s[i] = '9';
+            break;
+        }
+    }
+    return stoi(s);
+}
+int main() {
+    cout <<maximum69Number(9669);
+    return 0;
+}
