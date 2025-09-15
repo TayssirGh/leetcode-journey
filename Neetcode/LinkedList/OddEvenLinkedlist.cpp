e9f814a35f80bcd8abb600c20620a0fc1a82eaae
TayssirGh <tayssir.ghanmi@eniso.u-sousse.tn>
2025-09-15 19:56:21 +0200

    LEETCODE 75 -LL: Odd Even Linkedlist [https://leetcode.com/problems/odd-even-linked-list]

---
+++ Neetcode/LinkedList/OddEvenLinkedlist.cpp
@@ -0,0 +1,27 @@
+//
+// Created by tayssir on 9/15/25.
+//
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* oddEvenList(ListNode* head) {
+    if(head == nullptr || head->next == nullptr) return head;
+    ListNode* odd = head;
+    ListNode* even = head->next;
+    ListNode* evenHead = even;
+    while(even != nullptr && even->next != nullptr ) {
+        odd -> next = even->next;
+        odd = odd->next;
+        even -> next = even -> next -> next;
+        even = even -> next;
+    }
+    odd -> next = evenHead;
+    return head;
+
+
+}
