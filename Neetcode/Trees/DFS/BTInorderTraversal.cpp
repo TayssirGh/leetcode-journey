4069460f9cd0ad9dbef917f270310a092056a116
TayssirGh <tayssir.ghanmi@eniso.u-sousse.tn>
2025-09-21 22:53:56 +0200

    NEETCODE - Tree DFS: BT inorder traversal [https://leetcode.com/problems/binary-tree-inorder-traversal]

---
+++ Neetcode/Trees/DFS/BTInorderTraversal.cpp
@@ -0,0 +1,25 @@
+//
+// Created by tayssir on 9/21/25.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+vector<int> inorder( TreeNode* root, vector<int>& res) {
+    if (!root) return ;
+    inorder(root->left, res);
+    res.push_back(root->val);
+    inorder(root->right, res);
+
+}
+vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> res;
+    return inorder(root, res);
+}
