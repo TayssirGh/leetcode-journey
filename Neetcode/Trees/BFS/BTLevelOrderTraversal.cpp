3cb036024ffe33110818b8c503c6912a3f299ec7
TayssirGh <tayssir.ghanmi@eniso.u-sousse.tn>
2025-10-03 21:50:29 +0200

    NEETCODE -BT BFS: BT level order traversal [https://leetcode.com/problems/binary-tree-level-order-traversal/]

---
+++ Neetcode/Trees/BFS/BTLevelOrderTraversal.cpp
@@ -0,0 +1,42 @@
+//
+// Created by tayssir on 10/3/25.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> res;
+    queue<TreeNode*> q;
+    if (!root) {
+        return res;
+    }
+    // int treeLevel = 0;
+    q.push(root);
+    while (!q.empty()) {
+        int size = q.size();
+        vector<int> level;
+        for (int i = 0; i < size; i++) {
+            TreeNode* node = q.front();
+            level.push_back(node->val);
+            q.pop();
+            if (node->left) {
+                q.push(node->left);
+            }
+            if (node->right) {
+                q.push(node->right);
+            }
+
+        }
+        res.push_back(level);
+        // treeLevel++;
+    }
+    return res;
+}
