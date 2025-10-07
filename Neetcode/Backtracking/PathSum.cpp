e08c536090dcd5f01eef9d51a061d80ffdae5477
TayssirGh <tayssir.ghanmi@eniso.u-sousse.tn>
2025-10-07 20:02:45 +0200

    NEETCODE backtracking: path sum [https://leetcode.com/problems/path-sum/]

---
+++ Neetcode/Backtracking/PathSum.cpp
@@ -0,0 +1,22 @@
+//
+// Created by tayssir on 10/7/25.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+bool hasPathSum(TreeNode* root, int targetSum) {
+    if (!root) { return false; }
+    if (!root->left && !root->right) {
+        return targetSum == root->val;
+    }
+    targetSum -= root->val;
+    return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
+}
