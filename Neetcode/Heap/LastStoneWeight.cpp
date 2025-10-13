ee84fda68dd3ca344cc7600372905aa74d4c1366
TayssirGh <tayssir.ghanmi@eniso.u-sousse.tn>
2025-10-13 10:52:42 +0200

    NEETCODE -Heap: Last stone weight [https://leetcode.com/problems/last-stone-weight]

---
+++ Neetcode/Heap/LastStoneWeight.cpp
@@ -0,0 +1,17 @@
+//
+// Created by tayssir on 10/13/25.
+//
+#include <bits/stdc++.h>
+using namespace std;
+int lastStoneWeight(vector<int>& stones) {
+    priority_queue<int> pq(begin(stones), end(stones));
+    while (pq.size() > 1) {
+        int x = pq.top();
+        pq.pop();
+        int y = pq.top();
+        pq.pop();
+        if (x>y) pq.push(x-y);
+    }
+    return pq.empty()?0:pq.top();
+
+}
